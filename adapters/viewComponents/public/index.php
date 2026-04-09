00e55557971b8d4515101c4912c05fe1ced5da9a
nandordena <nandordena@gmail.com>
2026-04-09 21:38:51 +0200

    adds hold front request

--- adapters/viewComponents/public/index.php
+++ adapters/viewComponents/public/index.php
@@ -9,6 +9,12 @@ $_ADAPTER="VIEW_COMPONENTS";
 
 $uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 
+$endpoints = [
+    "components.workspace"
+    ,"form.null"
+    ,"layouts.null"
+];
+
 switch ($uri) {
     case '':
     case 'home':
@@ -20,6 +26,27 @@ switch ($uri) {
         break;
 
     default:
+        // Ahora, genera de manera escalable basado en el formato de $uri:
+        // ejemplo: "componets.workspace" -> "/ui/components/componets/workspace/index.php"
+        if (in_array($uri, $endpoints)) {
+            $parts = explode('.', $uri);
+            if (count($parts) == 2) {
+                $componentDir = $parts[0];
+                $componentFile = $parts[1];
+                $filePath = $BASEDIR . "/ui/{$componentDir}/{$componentFile}/index.php";
+                if (file_exists($filePath)) {
+                    include_once $filePath;
+                } else {
+                    echo "Component for endpoint '$uri' not found at $filePath.";
+                }
+                exit;
+            } else {
+                http_response_code(404);
+                echo '404';
+                exit;
+            }
+        }
         http_response_code(404);
         echo '404';
+        exit;
 }
--- adapters/viewComponents/public/ui/components/workspace/scripts.php
+++ adapters/viewComponents/public/ui/components/workspace/scripts.php
@@ -4,6 +4,13 @@
 
     window.Workspace.prototype.addNewSpace = function(e,data = []) {
         console.info([e,data]);
+        app.fetch("viewComponents", "components.workspace", [])
+            .then(response => {
+                console.info(response);
+            })
+            .catch(error => {
+                // handle error
+            });
     }
     window.Workspace.prototype.remove = function(e,data = []) {
         console.info([e,data]);
